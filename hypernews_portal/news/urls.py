638985f297f310cb4d8bf591f56012d6a3ea7724
Viktor Reshetnikov <reshviktor1@gmail.com>
2024-07-01 16:51:24 +0400

    hypernews portal stage 2/5

--- hypernews_portal/news/urls.py
+++ hypernews_portal/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('news/<int:link>/', views.NewsView.as_view(), name='news')
 ]
--- hypernews_portal/news/views.py
+++ hypernews_portal/news/views.py
@@ -1,7 +1,23 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.views import View
+import json
+from django.conf import settings
+
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class NewsView(View):
+    def get(self, request, link, *args, **kwargs):
+        with open(settings.NEWS_JSON_PATH) as json_file:
+            news_article = json.load(json_file)
+        articles = [article for article in news_article if article['link'] == link]
+        if not articles:
+            raise Http404("Resource not found")
+        article = articles[0]
+        return render(request, 'news_template.html', context={'news_article': article})
